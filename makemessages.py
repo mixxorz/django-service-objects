52af2462b1324d85e135716466cb405337a66c02
Breno Uchôa <brenouchoa+murdocb@gmail.com>
2017-08-17 01:04:26 -0300

    localization for pt_BR and base for other languages

---
+++ makemessages.py
@@ -0,0 +1,23 @@
+import sys
+import os
+import django
+from django.conf import settings
+from django.core import management
+
+BASE_DIR = os.path.dirname(__file__)
+settings.configure(
+    DATABASES={
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            }
+        },
+    LOCALE_PATHS=(
+        os.path.join(BASE_DIR, 'locale'),
+        ),
+
+    )
+
+django.setup()
+
+management.call_command('makemessages', "--locale=pt_BR", verbosity=0,
+                        interactive=False)
